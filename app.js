705b6c57122ae484ffe978413df5150c1650ada4
MuhammadZainAmmad <zainammad05@gmail.com>
2020-06-14 16:44:55 +0500

    SMIT Assignment 5 completed

---
+++ app.js
@@ -0,0 +1,63 @@
+//Chapter 1
+
+//Task#1
+// alert('Welcome to my web')
+
+// Task#2
+// alert("Error! Please enter a valid password")
+
+//Task#3
+// alert("Welcome to JS land.... \nHappy coding!") ;
+
+//Task#4
+// alert("Welcome to JS land....")
+// alert("Happy coding! \nPrevent this page from creating additional dialogs")
+
+//Task#5
+
+//Task#6
+//task#7
+
+
+
+//CHAPTER 2
+
+//Task#1
+// var username
+
+//Task#2
+// var myName='Muhammad Zain Ammad'
+
+//Task#3
+// message='Hello world'
+// alert(message)
+
+//Task#4
+// bioData=['Muhammad Zain Ammmad',20,"Future CIS engineer"]
+// for (var i=0;i<3;i++){
+//     if(i===0){
+//         alert('Name:\n' + bioData[i])
+//     }
+//     else if(i===1){
+//         alert('Age:\n' + bioData[i])
+//     }
+//     else if(i===2){
+//         alert('Motive:\n' + bioData[i])
+//     }
+// }
+
+//Task#5
+// food="Pizza"
+// alert(food+'\n'+food.slice(0,4)+'\n'+food.slice(0,3)+'\n'+food.slice(0,2)+'\n'+food[0])
+
+//Task#6
+// var email="zainammad05@gamil.com"
+// alert("My email address is "+email)
+
+// //Task#7
+// var book='A smarter way to learn JS'
+// alert('I am trying to learn from the Book '+book)
+
+// //Task#8
+// var abc="Yeah! I can write HTML content through JS"
+// document.write(abc)
